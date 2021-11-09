03e6afa56fdb7eeb2249c46dd3e8838ed46ad241
NSD0 <ht19a059@gmail.com>
2021-11-09 12:11:20 +0900

    commit

--- src/source.cpp
+++ src/source.cpp
@@ -3,13 +3,22 @@
 #define CPP2 PRIME UPPER LIMIT 1000000//’Tõ‚·‚é’l‚ÌãŒÀ’l
 
 int nth_prime(unsigned int a, unsigned int d, unsigned int n);
-bool check_number(unsigned int n);	// “ü—Í‚µ‚½©‘R”‚ª‘f”‚©‚Ç‚¤‚©”»’è‚·‚é
+bool is_prime(unsigned int n);	// “ü—Í‚µ‚½©‘R”‚ª‘f”‚©‚Ç‚¤‚©”»’è‚·‚é
 
 int main() {
 
 	std::cout << nth_prime(367, 186, 151) << std::endl;
-
-
+	std::cout << nth_prime(179, 10, 203) << std::endl;
+	std::cout << nth_prime(271, 37, 39) << std::endl;
+	std::cout << nth_prime(103, 230, 1) << std::endl;
+	std::cout << nth_prime(27, 104, 185) << std::endl;
+	std::cout << nth_prime(253, 50, 85) << std::endl;
+	std::cout << nth_prime(1, 1, 1) << std::endl;
+	std::cout << nth_prime(9075, 337, 210) << std::endl;
+	std::cout << nth_prime(307, 24, 79) << std::endl;
+	std::cout << nth_prime(331, 221, 177) << std::endl;
+	std::cout << nth_prime(259, 170, 40) << std::endl;
+	std::cout << nth_prime(269, 58, 102) << std::endl;
 
 	return 0;
 }
@@ -17,26 +26,25 @@ int main() {
 int nth_prime(unsigned int a, unsigned int d, unsigned int n)
 {
 
-	int i = 0;
-	int count = 1;	// “™·”—ñ a + d(n - 1) ‚Ìn
-	int item = a;	// “™·”—ñ a + d(n - 1) ‚Ìn€–Ú‚ğŠi”[‚·‚é•Ï”
+	unsigned int i = 1;		// “™·”—ñ a + d(n - 1) ‚Ìn
+	unsigned int count = 1;	// n”Ô–Ú‚Ì‘f”
+	unsigned int item = 0;	// “™·”—ñ a + d(n - 1) ‚Ìn€–Ú‚ğŠi”[‚·‚é•Ï”
 
 	do {
-		item = a + d * (count - 1);	// “™·”—ñ a + d(n - 1) ‚Ìn€–Ú
-		if (check_number(item)) {	// ‘f”‚©‚Ç‚¤‚©”»’è
-			i++;
+		item = a + (d * (i - 1));	// “™·”—ñ a + d(n - 1) ‚Ìn€–Ú
+		if (is_prime(item)) {	// ‘f”‚©‚Ç‚¤‚©”»’è
+			count++;
 		};
-		count++;
-	} while (i < n);	// n”Ô–Ú‚Ì‘f”
+		i++;
+	} while (count <= n);	// n”Ô–Ú‚Ì‘f”
 
 	return item;
 }
 
-bool check_number(unsigned int n)
+bool is_prime(unsigned int n)
 {
-
-	bool flag = false;	// Š„‚èØ‚ê‚½‚©”»’è
-	for (int i = 2;i < n; ++i) {
+	bool flag = false;	// Š„‚èØ‚ê‚½‚©‚ğ”»’è
+	for (int i = 2; i < n; i++) {
 		if (n % i == 0) {
 			flag = true;
 			break;
