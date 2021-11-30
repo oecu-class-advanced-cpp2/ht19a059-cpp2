d99c96cdaf2f03dee64eefc3b294a4ca6095763f
NSD0 <ht19a059@gmail.com>
2021-11-30 12:35:56 +0900

    スケルトンプログラムをビルドが通るように修正

--- ex-2-main.cpp
+++ ex-2-main.cpp
@@ -8,13 +8,52 @@ namespace cpp2 {
 	*/
 	class mcxi {
 	private:
-		std::string mcxi_to_Calc;
+		std::string mcx;
 	public:
+		mcxi(std::string x = "") { mcx = x; }
 
-		mcxi(std::string MCXI) {
-			mcxi_to_Calc = MCXI;
+		//MCXIをint型に変換
+		int mcxi_to_int(std::string mcxi) {
+			return 10;
 		};
+
+		//int型をMCXIに変換
+		std::string int_to_mcxi(int num) {
+			return "hogehoge";
+		};
+
+		//メンバにアクセス
+		std::string Get_mcx() {
+			return mcx;
+		}
+
+		//MCXIを返す
+		std::string to_string() {
+			return Get_mcx();
+		};
+
+		//+記号をオーバーロード
+		mcxi operator +(mcxi r)
+		{
+			mcxi tc;
+			int num;
+			num = mcxi_to_int(this->mcx) + mcxi_to_int(r.mcx);
+			tc.mcx = int_to_mcxi(num);
+			return tc;
+		};
+
+		//-記号をオーバーロード
+		mcxi operator -(mcxi r)
+		{
+			mcxi tc;
+			int num;
+			num = mcxi_to_int(this->mcx) - mcxi_to_int(r.mcx);
+			tc.mcx = int_to_mcxi(num);
+			return tc;
+		};
+
 	};
+	
 }
 
 int main() {
@@ -22,47 +61,47 @@ int main() {
 	cpp2::mcxi a0("xi");
 	cpp2::mcxi b0("x9i");
 	cpp2::mcxi result0 = a0 + b0;
-	std::cout << "3x" << "" << result0.to_string() << std::endl;
+	std::cout << "3x" << " " << result0.to_string() << std::endl;
 
 	cpp2::mcxi a1("i");
 	cpp2::mcxi b1("9i");
 	cpp2::mcxi result1 = a1 + b1;
-	std::cout << 'x' << " " << result1.to_string() << std::endl;
+	std::cout << "x" << " " << result1.to_string() << std::endl;
 
 	cpp2::mcxi a2("c2x2i");
 	cpp2::mcxi b2("4c8x8i");
 	cpp2::mcxi result2 = a2 + b2;
-	std::cout << '6cx' << " " << result2.to_string() << std::endl;
+	std::cout << "6cx" << " " << result2.to_string() << std::endl;
 
 	cpp2::mcxi a3("m2ci");
 	cpp2::mcxi b3("4m7c9x8i");
 	cpp2::mcxi result3 = a3 + b3;
-	std::cout << "5m9c9x9r" << " " << result3.to_string() << std::endl;
+	std::cout << "5m9c9x9i" << " " << result3.to_string() << std::endl;
 
 	cpp2::mcxi a4("9c9x9i");
 	cpp2::mcxi b4("i");
 	cpp2::mcxi result4 = a4 + b4;
-	std::cout << 'm' << " " << result4.to_string() << std::endl;
+	std::cout << "m" << " " << result4.to_string() << std::endl;
 
 	cpp2::mcxi a5("i");
-	cpp2::mcxi b5("9m9c9x8ir");
+	cpp2::mcxi b5("9m9c9x8i");
 	cpp2::mcxi result5 = a5 + b5;
-	std::cout << "9m9c9x9i"  " " << result5.to_string() << std::endl;
+	std::cout << "9m9c9x9i" << " " << result5.to_string() << std::endl;
 
 	cpp2::mcxi a6("m");
 	cpp2::mcxi b6("i");
 	cpp2::mcxi result6 = a6 + b6;
 	std::cout << "mi" << " " << result6.to_string() << std::endl;
 
-	cpp2::mcxi a7("2i");
+	cpp2::mcxi a7("i");
 	cpp2::mcxi b7("m");
 	cpp2::mcxi result7 = a7 + b7;
-	std::cout << "mi" <<" " << result7.to_string() << std::endl;
+	std::cout << "mi" << " " << result7.to_string() << std::endl;
 
-	cpp2::mcxi a8("m9in");
+	cpp2::mcxi a8("m9i");
 	cpp2::mcxi b8("i");
 	cpp2::mcxi result8 = a8 + b8;
-	std::cout << "mx" <<" " << result8.to_string() << std::endl;
+	std::cout << "mx" << " " << result8.to_string() << std::endl;
 
 	cpp2::mcxi a9("9m8c7xi");
 	cpp2::mcxi b9("c2x8i");
